d12a3af4fc966e97ab8f8634fa1646ca84aa1b49
Semhar Hidad <98850876+Semhar-Hidad@users.noreply.github.com>
2023-08-08 15:13:29 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+let dogAge = prompt("What is the age of your dog?");
+
+let humanAge = (dogAge -2) * 4 + 21
+
+alert("In human years, your dog is " + humanAge);
+
+document.querySelector('div').innerText += " Your dog is " + humanAge;
+
